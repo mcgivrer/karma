aa5a72043808606874c85235b86ba41f953505b7
Frédéric Delorme <frederic.delorme@gmail.com>
2024-01-17 18:01:13 +0100

    feat(acceleration): Add acceleration and forces to Entity
    - add acceleration and force a list to Entity class,
    - enhance the applyPhysic method tpo porcess forces and acceleration to be applied to velocity
    - define new config parameters for max velocity and max acceleration.

--- src/main/java/my/karma/app/scenes/PlayScene.java
+++ src/main/java/my/karma/app/scenes/PlayScene.java
@@ -46,7 +46,7 @@ public class PlayScene extends AbstractScene {
                 .setBorderColor(new Color(0.0f, 0.0f, 0.6f, 1.0f))
                 .setBackgroundColor(Color.BLUE)
                 .setPriority(1)
-                .setAttribute("speedStep", 0.15)
+                .setAttribute("speedStep", 0.015)
                 .setAttribute("energy", 100.0)
                 .setAttribute("mana", 100.0)
                 .setAttribute("hit", 1.0)
@@ -62,6 +62,7 @@ public class PlayScene extends AbstractScene {
     }
 
     private void createPlatforms(KarmaApp app) {
+        KarmaApp.Material platformMat = new KarmaApp.Material(1.0, 1.0, 0.1);
         KarmaApp.Entity platform1 = new KarmaApp.Entity("platform_01")
                 .setPosition(100, 100)
                 .setSize(100, 32)
@@ -71,7 +72,7 @@ public class PlayScene extends AbstractScene {
                 .setBorderColor(Color.GRAY)
                 .setBackgroundColor(Color.DARK_GRAY)
                 .setPriority(10)
-                .setMaterial(new KarmaApp.Material(1.0, 1.0, 0.1));
+                .setMaterial(platformMat);
         addEntity(platform1);
 
         KarmaApp.Entity platform2 = new KarmaApp.Entity("platform_border_top")
@@ -83,7 +84,7 @@ public class PlayScene extends AbstractScene {
                 .setBorderColor(Color.GRAY)
                 .setBackgroundColor(Color.DARK_GRAY)
                 .setPriority(10)
-                .setMaterial(new KarmaApp.Material(1.0, 1.0, 0.1));
+                .setMaterial(platformMat);
         addEntity(platform2);
 
         KarmaApp.Entity platform3 = new KarmaApp.Entity("platform_border_bottom")
@@ -95,19 +96,19 @@ public class PlayScene extends AbstractScene {
                 .setBorderColor(Color.GRAY)
                 .setBackgroundColor(Color.DARK_GRAY)
                 .setPriority(10)
-                .setMaterial(new KarmaApp.Material(1.0, 1.0, 0.35));
+                .setMaterial(platformMat);
         addEntity(platform3);
 
         KarmaApp.Entity platform4 = new KarmaApp.Entity("platform_border_left")
                 .setPosition(0, 16)
-                .setSize(16, (int) getWorld().getPlayArea().getHeight())
+                .setSize(16, (int) getWorld().getPlayArea().getHeight()-16)
                 .setPhysicType(KarmaApp.PhysicType.STATIC)
                 .setType(KarmaApp.EntityType.RECTANGLE)
                 .setMass(4.0)
                 .setBorderColor(Color.GRAY)
                 .setBackgroundColor(Color.DARK_GRAY)
                 .setPriority(10)
-                .setMaterial(new KarmaApp.Material(1.0, 1.0, 0.35));
+                .setMaterial(platformMat);
         addEntity(platform4);
 
         KarmaApp.Entity platform5 = new KarmaApp.Entity("platform_border_right")
@@ -119,7 +120,7 @@ public class PlayScene extends AbstractScene {
                 .setBorderColor(Color.GRAY)
                 .setBackgroundColor(Color.DARK_GRAY)
                 .setPriority(10)
-                .setMaterial(new KarmaApp.Material(1.0, 1.0, 0.35));
+                .setMaterial(platformMat);
         addEntity(platform5);
 
     }
