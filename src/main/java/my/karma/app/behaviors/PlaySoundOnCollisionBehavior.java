00030985f0624f59e61a4354e16fee91d80e9784
Frédéric Delorme <frederic.delorme@gmail.com>
2024-02-07 20:39:29 +0100

    refactor of some internal PlayScene function

--- src/main/java/my/karma/app/behaviors/PlaySoundOnCollisionBehavior.java
+++ src/main/java/my/karma/app/behaviors/PlaySoundOnCollisionBehavior.java
@@ -8,19 +8,21 @@ import java.util.List;
 public class PlaySoundOnCollisionBehavior implements KarmaPlatform.Behavior<KarmaPlatform.Entity> {
 
 
+    private String entityNameFiltering;
     private String soundName;
     private KarmaPlatform app;
 
     private List<KarmaPlatform.CollisionEvent> events = new ArrayList<>();
 
-    public PlaySoundOnCollisionBehavior(KarmaPlatform app, String soundName) {
+    public PlaySoundOnCollisionBehavior(KarmaPlatform app, String soundName, String entityNameFiltering) {
         this.app = app;
         this.soundName = soundName;
+        this.entityNameFiltering = entityNameFiltering;
     }
 
     @Override
     public void onCollision(KarmaPlatform.CollisionEvent ce) {
-        if (ce.getDst().name.startsWith("platform")) {
+        if (ce.getDst().name.contains(entityNameFiltering)) {
             if (!events.contains(ce)) {
                 events.add(ce);
                 KarmaPlatform.getSoundManager().playSound(soundName);
